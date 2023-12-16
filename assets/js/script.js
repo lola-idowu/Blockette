4631dc78d2faebd4d1bd99976b0f538c0304f7ed
lola-idowu <>
2023-12-16 14:12:16 +0000

    Code to fetch data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-const emojis = [ ]
+const gridContainer = document.querySelector(".grid-container");
+let cards = [];
+let firstCard, secondCard;
+let lockboard = false;
+let score = 0;
+
+document.querySelector(".score").textContent = score;
+
+fetch("./data/cards.json");
+    .then((res) => res.json())
+    .then((data) => {
+        cards = [...data, ...data];
+        shuffleCards();
+        generateCards();
+    });
+
+function shuffleCards
+
