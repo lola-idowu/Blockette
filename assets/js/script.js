45b69e30c79c038eb2d981c695fd0b764a7184d2
lola-idowu <>
2023-12-16 14:24:08 +0000

    Write generate cards function

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,5 +27,21 @@ function shuffleCards() {
     }
 }
 
-fu
+function generateCards() {
+    for (let card of cards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
+        cardElement.innerHTML = `
+            <div class= "front">
+                <img class="front-image" src=${card.image} />
+            </div>
+            <div class="back"></div>
+        `;
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
+    }
+}
+
+
 
