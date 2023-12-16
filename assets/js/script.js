fbf55c51f5027bfef599ef4a8a73aaff5f5139ce
lola-idowu <>
2023-12-16 14:01:03 +0000

    Add card images to file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,1 @@
-const gridContainer = document.querySelector(".grid-container");
-let cards = []
-let firstCard,secondCard;
-let lockboard = false;
-let score = 0;
-
-document .querySelector(".score").textContent = score;
-
-fetch("")
+const emojis = [ ]
